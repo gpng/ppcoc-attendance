51cf73025424532a55809fd5e6532f8bfcde60f0
Gerald <geraldpng@gmail.com>
2018-09-12 23:26:08 +0800

    basic setup for materia-ui with nextjs

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,27 +1,63 @@
-import App, { Container } from 'next/app';
 import React from 'react';
-import basicCSS from '../stylesheets/basic';
+import App, { Container } from 'next/app';
+import JssProvider from 'react-jss/lib/JssProvider';
 
-export default class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
+// material-ui
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import getPageContext from '../components/getPageContext';
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
+class MyApp extends App {
+  constructor(props) {
+    super(props);
+    this.pageContext = getPageContext();
+  }
+
+  pageContext = null;
+
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
     }
-
-    return { pageProps };
   }
 
   render() {
     const { Component, pageProps } = this.props;
     return (
       <Container>
-        <Component {...pageProps} />
-        <style global jsx>
-          {basicCSS}
-        </style>
+        {/* Wrap every page in Jss and Theme providers */}
+        <JssProvider
+          registry={this.pageContext.sheetsRegistry}
+          generateClassName={this.pageContext.generateClassName}
+        >
+          {/* MuiThemeProvider makes the theme available down the React
+              tree thanks to React context. */}
+          <MuiThemeProvider
+            theme={this.pageContext.theme}
+            sheetsManager={this.pageContext.sheetsManager}
+          >
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            {/* Pass pageContext to the _document though the renderPage enhancer
+                to render collected styles on server side. */}
+            <AppBar position="static" color="primary">
+              <Toolbar>
+                <Typography variant="title" color="inherit">
+                  PPCOC
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <Component pageContext={this.pageContext} {...pageProps} />
+          </MuiThemeProvider>
+        </JssProvider>
       </Container>
     );
   }
 }
+
+export default MyApp;
