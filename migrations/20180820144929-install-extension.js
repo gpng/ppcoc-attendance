af622d610b7c5e4dccf015f4f297dcce27d804e7
Gerald <geraldpng@gmail.com>
2018-09-14 23:57:24 +0800

    create extension only if it doesn't exist

--- migrations/20180820144929-install-extension.js
+++ migrations/20180820144929-install-extension.js
@@ -1,4 +1,4 @@
 module.exports = {
-  up: queryInterface => queryInterface.sequelize.query('CREATE EXTENSION pg_trgm;'),
+  up: queryInterface => queryInterface.sequelize.query('CREATE EXTENSION IF NOT EXISTS pg_trgm;'),
   down: queryInterface => queryInterface.sequelize.query('DROP EXTENSION pg_trgm;'),
 };
