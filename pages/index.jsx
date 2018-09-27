4c3689f09564f01b963fde43ee1542cf2df2794b
Gerald <geraldpng@gmail.com>
2018-09-27 22:49:57 +0800

    added welcome message

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import classNames from 'classnames';
 
 // material-ui
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
 // styles
+import Typography from '@material-ui/core/Typography';
 import { container } from '../stylesheets/general';
 
 const styles = {
@@ -15,16 +17,26 @@ const styles = {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+    paddingTop: '12px',
   },
   button: {
     width: '50%',
     minWidth: '50%',
     margin: '16px 0',
   },
+  message: {
+    fontSize: '1em',
+    textAlign: 'center',
+  },
+  messageBottom: {
+    marginTop: '12px',
+  },
 };
 
 const Index = ({ classes }) => (
   <div className={classes.root}>
+    <Typography className={classes.message}>Welcome to our Worship Services!</Typography>
+    <Typography className={classes.message}>Let us worship God in Spirit and in Truth.</Typography>
     <Link prefetch href="members">
       <Button className={classes.button} variant="contained" color="secondary">
         Members
@@ -35,6 +47,9 @@ const Index = ({ classes }) => (
         Visitors
       </Button>
     </Link>
+    <Typography className={classNames(classes.message, classes.messageBottom)}>
+      Enter to Worship, Leave to Serve.
+    </Typography>
   </div>
 );
 
